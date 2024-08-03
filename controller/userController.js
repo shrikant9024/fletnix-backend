e91ad987ad35250e4f5b6567419bb988d5fd92a3
Shrikant Ramani <shrikantramani@Shrikants-MacBook-Air.local>
2024-08-03 12:23:03 +0530

    .env config

--- controller/userController.js
+++ controller/userController.js
@@ -1,57 +1,69 @@
-const User = require("../models/user")
-const bcrypt = require("bcrypt")
-const cookieParser = require("cookie-parser");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 const { createToken } = require("../service/jwt");
-const { error } = require("console");
 
+const saltRounds = 10; // Use a more descriptive variable name
 
+// Handle user signup
+async function handleSignup(req, res) {
+    try {
+        const { email, name, password, age } = req.body;
 
-
-async function handleSignup(req,res){
-    try{
-        const {email,name,password,age}=req.body
-        
-        //user exist
-    const existingUser = await User.findOne({email});
-    if(existingUser){
-        return res.status(400).send("User already exists")
-    }
-    const hashedPassword= await bcrypt.hash(password,10);
-
-    const result = await User.create({
-        email, name, password:hashedPassword,age
-    })
-
-    return res.status(201).json({msg:"user registration succesfull"})
-    }catch(error){
-        console.error("error while signing up",error)
-        return res.status(500).json({ error: "Internal server error" });
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).send("User already exists");
         }
 
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        // Create a new user
+        const newUser = await User.create({
+            email,
+            name,
+            password: hashedPassword,
+            age
+        });
+
+        return res.status(201).json({ msg: "User registration successful" });
+    } catch (error) {
+        console.error("Error while signing up:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
 }
 
+// Handle user login
+async function handleLogin(req, res) {
+    const { email, password } = req.body;
 
-async function handleLogin(req,res){
-    const {email,password} = req.body
+    try {
+        // Find the user
+        const loginUser = await User.findOne({ email });
+        if (!loginUser) {
+            return res.status(400).send("User does not exist");
+        }
 
-        const loginUser = await User.findOne({email});
-        if(!loginUser) res.status(400).send("User does not exist")
+        // Compare passwords
+        const match = await bcrypt.compare(password, loginUser.password);
+        if (!match) {
+            return res.status(401).json({ error: "Wrong password" });
+        }
 
-            const dbpassword = loginUser.password
-            bcrypt.compare(password,dbpassword).then((match)=>{
-                if(!match){
-                    res.status(401).json({error:"wrong password"})
-                }
-            })
-            const accessToken = createToken(User)
-            res.cookie("jwt",accessToken,{
-                expire: new Date(Date.now()+7*24*60*60*1000),
-            })
-      
-            return res.status(200).json({msg:"user logged in succesfully"})
-            }
-          
-        
+        // Create a JWT token
+        const accessToken = createToken({ id: loginUser._id });
 
+        // Send the token in a cookie
+        res.cookie("jwt", accessToken, {
+            httpOnly: true, // This should be true for security
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 7 days
+        });
 
-module.exports = {handleLogin,handleSignup}
+        return res.status(200).json({ msg: "User logged in successfully" });
+    } catch (error) {
+        console.error("Error while logging in:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+module.exports = { handleLogin, handleSignup };
--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const router = require('./routes/route')
 const { mongo, default: mongoose } = require('mongoose')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+require("dotenv").config(); 
 
 const app = express()
 const PORT = 8000
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017/fletnix").then(()=>console.log('mongodb connected')).catch(err=>console.log('Mongo error',err))
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log('mongodb connected')).catch(err=>console.log('Mongo error',err))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
@@ -18,3 +19,5 @@ app.use("/",router)
 
 
 app.listen(PORT,()=>console.log(`Server is running at ${PORT}`))
+
+// mongodb+srv://fletnix:Mongo4527@cluster0.apm1ods.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
